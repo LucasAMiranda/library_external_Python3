d5070ad799494eb606bd6441719a98e19d649a96
LucasAMiranda <lucasa.miranda@hotmail.com>
2021-04-06 22:15:03 -0300

    Modified geolocation conversion

--- 5_1_Wazeyes/Converter_Geolocalizacao.py
+++ 5_1_Wazeyes/Converter_Geolocalizacao.py
@@ -1,19 +1,20 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="Wazeyes") #Nome do seu app
+from pygeocoders import Geocoder
+geolocator = Geocoder(user_agent="Wazeyes") #Nome do seu app
 
-endereco = input("Digite um endereço com o número  e cidade: ")
+endereco = {}
+
+#armazena os inputs em uma lista de dicionário de dados
+endereco = [input("Digite um endereço com o número  e cidade: ")]
 
 resultado = geolocator.gecode(endereco)
 
-if resultado.valid_address == True:
-    print("Endereço completo.: ", resultado)
-    print("Coordenadas........: ", resultado.coordinates)
-    print("Número.............: ", resultado.street_number)
-    print("CEP................: ", resultado.postal_code)
+if resultado.valid_address == True:  #Mostra as coordenadas do endereço completo pela key(chave)
+    print("Endereço completo..: ", (Geocoder('AIzaSyAR3KdmvdCKEx6Obe_-G-FUS053OccVo0g').geocode(resultado).coordinates)
 
-print("Foram encontrados ", resultado.count, "endereços.")
 for r in resultado:
-    print("Cidade........: ", r.state)
-    print("País..........: ", r.country)
-    print("Logradouro....: ", r.route)
-    print("############################")
+    print(f"Número........: ", {r.street_number})
+    print(f"Cidade........: ", {r.state})
+    print(f"País..........: ", {r.country})
+    print(f"Logradouro....: ", {r.route})
+
+print("Foi(ram) encontrado(s) ",resultado.count, " endereço(s).")
